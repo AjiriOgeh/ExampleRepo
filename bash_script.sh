67c64016b394102a07012925966381a233265863
AjiriOgeh <ajiogeh@gmail.com>
2024-08-27 11:19:31 +0100

    initial Commit

---
+++ bash_script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Hello"
+read -p "Enter repository name: " repository
+
+mkdir $repository
+cd $repository
+mkdir .github
+cd .github
+mkdir workflows
+touch workflows.yml
+cd ..
+cd ..
+gh repo create $repository --public
+echo $repository >> README.md
+git init
+git add .
+git commit -m "initial Commit"
+git branch -M main
+git remote add origin https://github.com/AjiriOgeh/$repository.git
+git push -u origin main
+git branch in-dev
+git push origin in-dev
